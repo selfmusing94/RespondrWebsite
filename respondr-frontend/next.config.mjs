163ac54bea5c81f134cff9f845fb08d0e7bf5a74
selfmusing94 <selfmusing9@gmail.com>
2025-05-20 02:12:17 +0530

    Configured to deploy on Netlify

--- respondr-frontend/next.config.mjs
+++ respondr-frontend/next.config.mjs
@@ -10,12 +10,5 @@ const nextConfig = {
     unoptimized: true,
   },
 }
-// next.config.js
-const { withNetlify } = require('@netlify/next');
-
-module.exports = withNetlify({
-  reactStrictMode: true,
-});
-
 
 export default nextConfig
